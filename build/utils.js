07cc3e5f96d0729193929414b5ca234d0fb09448
shadow <822599633@qq.com>
2016-12-29 07:43:42 +0800

    调整打包编译框架

---
+++ build/utils.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var config = require('../config');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+exports.assetsPath = function (_path) {
+    var assetsSubDirectory = process.env.NODE_ENV === 'production'
+        ? config.build.assetsSubDirectory
+        : config.dev.assetsSubDirectory;
+    return path.posix.join(assetsSubDirectory, _path);
+};
+
+exports.cssLoaders = function (env) {
+    var isProd = (env === 'production');
+    var cssLoaders = [{
+        test: /\.css$/,
+        loader: isProd
+            ? ExtractTextPlugin.extract('style', 'css!postcss')
+            : 'style!css!postcss'
+    }];
+    if (config.cssLoaders.indexOf('less') !== -1) {
+        cssLoaders.push({
+            test: /\.less$/,
+            loader: isProd
+                ? ExtractTextPlugin.extract('style', 'css!less!postcss')
+                : 'style!css!less!postcss'
+        });
+    };
+    if (config.cssLoaders.indexOf('scss') !== -1 || config.cssLoaders.indexOf('sass') !== -1) {
+        cssLoaders.push({
+            test: /\.(scss|sass)$/,
+            loader: isProd
+                ? ExtractTextPlugin.extract('style', 'css!sass!postcss')
+                : 'style!css!sass!postcss'
+        });
+    };
+    return cssLoaders;
+}
